322a2451dae6c7831d1a8b931275a7f78147c888
marocchino <marocchino@users.noreply.github.com>
2021-03-27 12:13:51 +0900

    refactor: several improvements (#262)
    - more tests
    - file read in config
    - reduce if depth
    - reduce unnecessary requests

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -1,18 +1,127 @@
-import * as process from 'process'
-
-test('test runs', () => {
-  process.env['INPUT_HEADER'] = ''
+beforeEach(() => {
+  process.env['GITHUB_REPOSITORY'] = 'marocchino/stick-pull-request-comment'
   process.env['INPUT_NUMBER'] = '123'
   process.env['INPUT_APPEND'] = 'false'
   process.env['INPUT_RECREATE'] = 'false'
   process.env['INPUT_DELETE'] = 'false'
   process.env['INPUT_GITHUB_TOKEN'] = 'some-token'
-  process.env['GITHUB_REPOSITORY'] = 'marocchino/stick-pull-request-comment'
+})
+
+afterEach(() => {
+  jest.resetModules()
+  delete process.env['GITHUB_REPOSITORY']
+  delete process.env['INPUT_REPO']
+  delete process.env['INPUT_HEADER']
+  delete process.env['INPUT_MESSAGE']
+  delete process.env['INPUT_NUMBER']
+  delete process.env['INPUT_APPEND']
+  delete process.env['INPUT_RECREATE']
+  delete process.env['INPUT_DELETE']
+  delete process.env['INPUT_GITHUB_TOKEN']
+  delete process.env['INPUT_PATH']
+})
+
+test('repo', () => {
+  process.env['INPUT_REPO'] = 'other'
+  expect(require('../src/config')).toMatchObject({
+    pullRequestNumber: expect.any(Number),
+    repo: {owner: 'marocchino', repo: 'other'},
+    body: '',
+    header: '',
+    append: false,
+    recreate: false,
+    deleteOldComment: false,
+    githubToken: 'some-token'
+  })
+})
+test('header', () => {
+  process.env['INPUT_HEADER'] = 'header'
   expect(require('../src/config')).toMatchObject({
     pullRequestNumber: expect.any(Number),
     repo: {owner: 'marocchino', repo: 'stick-pull-request-comment'},
-    message: '',
-    path: '',
+    body: '',
+    header: 'header',
+    append: false,
+    recreate: false,
+    deleteOldComment: false,
+    githubToken: 'some-token'
+  })
+})
+test('append', () => {
+  process.env['INPUT_APPEND'] = 'true'
+  expect(require('../src/config')).toMatchObject({
+    pullRequestNumber: expect.any(Number),
+    repo: {owner: 'marocchino', repo: 'stick-pull-request-comment'},
+    body: '',
+    header: '',
+    append: true,
+    recreate: false,
+    deleteOldComment: false,
+    githubToken: 'some-token'
+  })
+})
+test('recreate', () => {
+  process.env['INPUT_RECREATE'] = 'true'
+  expect(require('../src/config')).toMatchObject({
+    pullRequestNumber: expect.any(Number),
+    repo: {owner: 'marocchino', repo: 'stick-pull-request-comment'},
+    body: '',
+    header: '',
+    append: false,
+    recreate: true,
+    deleteOldComment: false,
+    githubToken: 'some-token'
+  })
+})
+test('delete', () => {
+  process.env['INPUT_DELETE'] = 'true'
+  expect(require('../src/config')).toMatchObject({
+    pullRequestNumber: expect.any(Number),
+    repo: {owner: 'marocchino', repo: 'stick-pull-request-comment'},
+    body: '',
+    header: '',
+    append: false,
+    recreate: false,
+    deleteOldComment: true,
+    githubToken: 'some-token'
+  })
+})
+describe('path', () => {
+  test('when exists return content of a file', () => {
+    process.env['INPUT_PATH'] = './__tests__/assets/result'
+    expect(require('../src/config')).toMatchObject({
+      pullRequestNumber: expect.any(Number),
+      repo: {owner: 'marocchino', repo: 'stick-pull-request-comment'},
+      body: 'hi there\n',
+      header: '',
+      append: false,
+      recreate: false,
+      deleteOldComment: false,
+      githubToken: 'some-token'
+    })
+  })
+
+  test('when not exists return null string', () => {
+    process.env['INPUT_PATH'] = './__tests__/assets/not_exists'
+    expect(require('../src/config')).toMatchObject({
+      pullRequestNumber: expect.any(Number),
+      repo: {owner: 'marocchino', repo: 'stick-pull-request-comment'},
+      body: '',
+      header: '',
+      append: false,
+      recreate: false,
+      deleteOldComment: false,
+      githubToken: 'some-token'
+    })
+  })
+})
+
+test('message', () => {
+  process.env['INPUT_MESSAGE'] = 'hello there'
+  expect(require('../src/config')).toMatchObject({
+    pullRequestNumber: expect.any(Number),
+    repo: {owner: 'marocchino', repo: 'stick-pull-request-comment'},
+    body: 'hello there',
     header: '',
     append: false,
     recreate: false,
--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 import * as core from '@actions/core'
 import {context} from '@actions/github'
+import {readFileSync} from 'fs'
 
 export const pullRequestNumber =
   context?.payload?.pull_request?.number ||
   +core.getInput('number', {required: false})
 
 export const repo = buildRepo()
-export const message = core.getInput('message', {required: false})
-export const path = core.getInput('path', {required: false})
 export const header = core.getInput('header', {required: false})
 export const append = core.getInput('append', {required: true}) === 'true'
 export const recreate = core.getInput('recreate', {required: true}) === 'true'
@@ -15,9 +14,25 @@ export const deleteOldComment =
   core.getInput('delete', {required: true}) === 'true'
 export const githubToken = core.getInput('GITHUB_TOKEN', {required: true})
 
+export const body = buildBody()
+
 function buildRepo(): {repo: string; owner: string} {
   return {
     owner: context.repo.owner,
     repo: core.getInput('repo', {required: false}) || context.repo.repo
+  }
+}
+
+function buildBody(): string {
+  const path = core.getInput('path', {required: false})
+  if (path) {
+    try {
+      return readFileSync(path, 'utf-8')
+    } catch (error) {
+      core.setFailed(error.message)
+      return ''
+    }
+  } else {
+    return core.getInput('message', {required: false})
   }
 }
