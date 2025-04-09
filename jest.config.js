1125c50abda416ff579c4ea5bfc5b0ddea466f2d
marocchino <marocchino@users.noreply.github.com>
2025-04-09 22:06:51 +0900

    🔧 Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,24 @@
 module.exports = {
   clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  moduleFileExtensions: ['js', 'ts', 'json', 'node'],
+  testEnvironment: "jest-environment-node",
+  testMatch: ["**/__tests__/**/*.test.ts"],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    "^.+\\.ts$": ["@swc/jest", {
+      jsc: {
+        parser: {
+          syntax: "typescript",
+          tsx: false
+        },
+        target: "es2022"
+      }
+    }]
   },
-  verbose: true
+  verbose: true,
+  testTimeout: 10000,
+  maxWorkers: "50%"
 }
